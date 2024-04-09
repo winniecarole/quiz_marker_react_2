280624ca615c521e9634eace01920970791d6a80
winniecarole <winniedjoukeng@gmail.com>
2024-04-09 15:24:37 +0200

    DEPLOY

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import NavBar from "./components/navbar";
 import QuizMarker from "./components/quizMarker";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
