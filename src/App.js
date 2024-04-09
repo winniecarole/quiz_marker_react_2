e9d57b07eccecff582fddd7cfbe1b880999939d9
winniecarole <winniedjoukeng@gmail.com>
2024-04-09 16:37:24 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
 import NavBar from "./components/navbar";
 import QuizMarker from "./components/quizMarker";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import QuizResults from "./components/quizResults";
 
 function App() {
   return (
     <>
         <NavBar/>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<QuizMarker/>} />
                 <Route path="/results" element={<QuizResults />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </>
   );
 }
