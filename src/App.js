5437a97d8300c948e2fb300763691ee41eed202e
winniecarole <winniedjoukeng@gmail.com>
2024-04-09 16:20:23 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
   return (
     <>
         <NavBar/>
-        <QuizMarker/>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<QuizMarker/>} />
